132739223d7695c00980f40431ea9eb2edb0c57a
acoutts <andrewjohncoutts@gmail.com>
2018-06-16 11:25:42 -0400

    Refactor getEosPrice()
    Move getEosPrice() into separate code block

--- backend/src/com/eosrp/PriceDbWriter.java
+++ backend/src/com/eosrp/PriceDbWriter.java
@@ -13,11 +13,28 @@ import com.eosrp.network.*;
 
 public class PriceDbWriter {	
 	
+	private static double getEosPrice() throws IOException {
+		String urlEosPriceUsd = "https://api.coinmarketcap.com/v2/ticker/1765/";
+		Double eosPriceUsd;
+		
+		//~ Get request test ~//
+		HttpGetHelper getTest = new HttpGetHelper(urlEosPriceUsd);
+		String jsonString = getTest.sendRequest();
+		
+		//~ Get USD price of EOS ~//
+		JSONObject jsonObject = (JSONObject) JSONValue.parse(jsonString);
+		JSONObject data = (JSONObject) jsonObject.get("data");
+		JSONObject quotes = (JSONObject) data.get("quotes");
+		JSONObject usd = (JSONObject) quotes.get("USD");
+		eosPriceUsd = (Double) usd.get("price"); 
+		System.out.println(eosPriceUsd);
+		return eosPriceUsd;
+	}
+	
 	public static void main(String[] args) throws SQLException, IOException {
 		
 		String urlGetTableRows = "http://node1.eosphere.io:8888";
 		String urlGetAccount = "http://node1.eosphere.io:8888/v1/chain/get_account";
-		String urlEosPriceUsd = "https://api.coinmarketcap.com/v2/ticker/1765/";
 		
 		
 		//~ Initialize DB connection ~//
@@ -35,18 +52,17 @@ public class PriceDbWriter {
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		//----------//
 		
 		//~ Get timestamp ~//
 		java.util.Date date = new java.util.Date(System.currentTimeMillis());
 		java.sql.Timestamp dt = new java.sql.Timestamp(date.getTime());
-		//----------//
 
+
+		
 		//~ Attempt to insert record ~//
-		if (client.insert("eoscpu", dt, 0.01554462, 0.171) == 1) {
+		if (client.insert("eoscpu", dt, 0.01554462, getEosPrice()) == 1) {
 			System.out.println("Record added");
 		}
-		//----------//
 
 		//~ Print result ~//
 		ResultSet rs = client.execQuery("SELECT * FROM eosram");
@@ -56,18 +72,6 @@ public class PriceDbWriter {
 					rs.getString(2),
 					rs.getString(3));
 		}
-		//----------//
-		
-		//~ Get request test ~//
-		HttpGetHelper getTest = new HttpGetHelper(urlEosPriceUsd);
-		String jsonString = getTest.sendRequest();
-		
-		JSONObject jsonObject = (JSONObject) JSONValue.parse(jsonString);
-		
-		JSONObject data = (JSONObject) jsonObject.get("data");
-		String symbol = (String) data.get("symbol");
-		
-		System.out.println(symbol);
 
 	}
 
