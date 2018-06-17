609a672989ab57f20634d8e47928545096fecf0c
acoutts <andrewjohncoutts@gmail.com>
2018-06-16 20:23:56 -0400

    Initial commit for CPU price
    Successful testing parsing string object out of the get_account response json to get current cpu_weight

--- backend/src/com/eosrp/PriceDbWriter.java
+++ backend/src/com/eosrp/PriceDbWriter.java
@@ -10,57 +10,93 @@ import com.eosrp.network.*;
 
 public class PriceDbWriter {	
 
+	//~ Get the current price of CPU bandwidth in EOS/ms
+	private static double getCpuPriceMs(String _url) throws IOException {
+		String strAccountInfoJson = "{\"account_name\":\"eosnewyorkio\"}";
+
+		//~ Instantiate the POST request object 
+		//~ and fetch latest price for CPU
+		HttpPostHelper postReq = new HttpPostHelper(_url, strAccountInfoJson);
+		JSONObject _jsonResp = postReq.sendRequest();
+
+		//~ We need to access 'quote' and 'base' to get the balance values
+		JSONObject strTotalResources = (JSONObject) _jsonResp.get("total_resources");
+		String strCpuWeight = (String) strTotalResources.get("cpu_weight");
+		System.out.println(strCpuWeight);
+		
+		/*
+		JSONObject strBase = (JSONObject) strRow.get("base");
+
+		//~ Get the balance values
+		String strQuoteBalance = (String) strQuote.get("balance").toString();
+		String strBaseBalance = (String) strBase.get("balance").toString();
+
+		//~ The balance value is a string with a unit on the end, select only the numerical part
+		String strTrimmedQuoteBalance = strQuoteBalance.substring(0, strQuoteBalance.indexOf(' '));
+		String strTrimmedBaseBalance = strBaseBalance.substring(0, strBaseBalance.indexOf(' '));
+
+		//~ Now cast it into a double so we can do math on it
+		Double dblQuoteBalance = Double.parseDouble(strTrimmedQuoteBalance);
+		Double dblBaseBalance = Double.parseDouble(strTrimmedBaseBalance);
+
+		//~ Finaly calculate the RAM/EOS price before returning it
+		_ramPriceEos = dblQuoteBalance / dblBaseBalance;
+*/
+		return 0;
+
+	}
+
 	//~ Get current price per byte (RAM/EOS) for next byte of RAM
 	//~ Note this does not include the price slippage or 0.5% fee
 	//~ Todo: use Bancor algorithm to calculate exact price of X bytes
 	private static double getRamPriceBytes(String _url) throws IOException {
 		Double _ramPriceEos;
-		
+
 		//~ This is from running the following command in cleos to get the JSON payload:
 		//~ cleos --print-response --print-request get table eosio eosio rammarket
-		String ramMarketJson = "{\"json\":\"true\",\"code\":\"eosio\",\"scope\":\"eosio\",\"table\":\"rammarket\",\"limit\":\"10\"}";
-		
+		String strRamMarketJson = "{\"json\":\"true\",\"code\":\"eosio\",\"scope\":\"eosio\",\"table\":\"rammarket\",\"limit\":\"10\"}";
+
 		//~ DEBUG
 		//~ System.out.println("reqJson: " + ramMarketJson);
-		
+
 		//~ Instantiate the POST request object 
 		//~ and fetch latest price for ram
-		HttpPostHelper postReq = new HttpPostHelper(_url, ramMarketJson);
+		HttpPostHelper postReq = new HttpPostHelper(_url, strRamMarketJson);
 		JSONObject _jsonResp = postReq.sendRequest();
-		
+
 		//~ The JSON response looks like this:
 		//~ {"rows":[{"supply":"10000000000.0000 RAMCORE","base":{"balance":"67172012846 RAM","weight":"0.50000000000000000"},"quote":{"balance":"1023040.5170 EOS","weight":"0.50000000000000000"}}],"more":false}
 		//~ First use a JSONArray to read the first (only) row in the data
 		JSONArray rowsArray = (JSONArray) _jsonResp.get("rows");
 		JSONObject strRow = (JSONObject) rowsArray.get(0);
-		
+
 		//~ We need to access 'quote' and 'base' to get the balance values
 		JSONObject strQuote = (JSONObject) strRow.get("quote");
 		JSONObject strBase = (JSONObject) strRow.get("base");
-		
+
 		//~ Get the balance values
 		String strQuoteBalance = (String) strQuote.get("balance").toString();
 		String strBaseBalance = (String) strBase.get("balance").toString();
-		
+
 		//~ The balance value is a string with a unit on the end, select only the numerical part
 		String strTrimmedQuoteBalance = strQuoteBalance.substring(0, strQuoteBalance.indexOf(' '));
 		String strTrimmedBaseBalance = strBaseBalance.substring(0, strBaseBalance.indexOf(' '));
-		
+
 		//~ Now cast it into a double so we can do math on it
 		Double dblQuoteBalance = Double.parseDouble(strTrimmedQuoteBalance);
 		Double dblBaseBalance = Double.parseDouble(strTrimmedBaseBalance);
-		
+
 		//~ Finaly calculate the RAM/EOS price before returning it
 		_ramPriceEos = dblQuoteBalance / dblBaseBalance;
-		
+
 		//~ DEBUG
 		//~ System.out.printf("Quote Balance: %f\n", dblQuoteBalance);
 		//~ System.out.printf("Base Balance: %f\n", dblBaseBalance);
 		//~ System.out.printf("Price: %.8f",  _ramPriceEos);
-		
+
 		return _ramPriceEos;
 	}
-	
+
 	//~ Get the current EOS/USD price from coinmarketcap's API
 	private static double getEosPriceUsd(String _url) throws IOException {
 		Double _eosPriceUsd;
@@ -68,7 +104,7 @@ public class PriceDbWriter {
 		//~ Instantiate the GET request object 
 		//~ and fetch latest price for EOS/USD
 		HttpGetHelper getReq = new HttpGetHelper(_url);
-		
+
 		//~ Capture request response JSON in a JSON object
 		JSONObject _jsonResp = getReq.sendRequest();
 
@@ -77,10 +113,10 @@ public class PriceDbWriter {
 		JSONObject quotes = (JSONObject) data.get("quotes");
 		JSONObject usd = (JSONObject) quotes.get("USD");
 		_eosPriceUsd = (Double) usd.get("price"); 
-		
+
 		//~ DEBUG
 		//~ System.out.println(_eosPriceUsd);
-		
+
 		return _eosPriceUsd;
 	}
 
@@ -90,7 +126,7 @@ public class PriceDbWriter {
 		java.util.Date date = new java.util.Date(System.currentTimeMillis());
 		java.sql.Timestamp dt = new java.sql.Timestamp(date.getTime());
 
-		
+
 		//~ Attempt to insert record
 		//~ DEBUG
 		//~ System.out.printf("Table: %s,  dt: %s, _eosPriceUsd: %s, _resourcePriceEos: %s\n", _table, dt, _eosPriceUsd, _resourcePriceEos);
@@ -101,7 +137,7 @@ public class PriceDbWriter {
 		}
 		return 0;
 	}
-	
+
 	private static PostgresHelper initDatabase() {
 		//~ Initialize DB connection
 		PostgresHelper client = new PostgresHelper(
@@ -122,27 +158,13 @@ public class PriceDbWriter {
 		return client;
 	}
 
-	public static void main(String[] args) throws SQLException, IOException {
-		//~ Backup node: node1.eosphere.io:8888
+	public static void updateRamPrice() throws SQLException, IOException {
+		PostgresHelper client = initDatabase();
 		String strUrlEosPriceUsd = "https://api.coinmarketcap.com/v2/ticker/1765";
 		String strUrlGetTableRows = "http://api.eosnewyork.io/v1/chain/get_table_rows";
-		String strUrlGetAccount = "http://api.eosnewyork.io/v1/chain/get_account";
 
-		//~ Initialize DB connection
-		PostgresHelper client = initDatabase();
-		
-		//~ Get the latest prices
-		Double eosRamPrice = getRamPriceBytes(strUrlGetTableRows);
-		Double eosUsdPrice = getEosPriceUsd(strUrlEosPriceUsd);
-		
-		//~ DEBUG
-		//~ System.out.println(eosRamPrice);
-		//~ System.out.println(eosUsdPrice);
-		
-		
-		//~ Test query to put the latest EOS ram price in
 		if (client != null) {
-			int result = sendQuery("eosram", eosUsdPrice, client, eosRamPrice);
+			int result = sendQuery("eosram", getEosPriceUsd(strUrlEosPriceUsd), client, getRamPriceBytes(strUrlGetTableRows));
 			if (result == 1) {
 				//~ DEBUG
 				//~ System.out.println("Successful query");
@@ -150,7 +172,7 @@ public class PriceDbWriter {
 			else
 			{
 				//~ DEBUG
-				//~ System.out.println("Unsuccessful querty");
+				//~ System.out.println("Unsuccessful query");
 			}
 		}
 
@@ -162,7 +184,22 @@ public class PriceDbWriter {
 					rs.getString(2),
 					rs.getString(3));
 		}
+	}
 
+	public static void updateNetPrice() {
+
+	}
+
+	public static void updateCpuPrice() {
+
+	}
+
+	public static void main(String[] args) throws SQLException, IOException {
+		//~ Backup node: node1.eosphere.io:8888
+		String strUrlGetAccount = "http://api.eosnewyork.io/v1/chain/get_account";
+
+		//~ updateRamPrice();
+		getCpuPriceMs(strUrlGetAccount);
 	}
 
 }
--- frontend/js/jquery.isotope.load.js
+++ frontend/js/jquery.isotope.load.js
@@ -102,7 +102,6 @@ jQuery(window).load(function($) {
     ramBaseBalance = ramBaseBalance.substr(0,ramBaseBalance.indexOf(' '));
     var ramQuoteBalance = xDoc.rows[0].quote.balance; // Amount of EOS in the RAM collector
     ramQuoteBalance = ramQuoteBalance.substr(0,ramQuoteBalance.indexOf(' '));
-    var ramUsed = 1 - (ramBaseBalance - maxRam);
     ramPriceEos = ((ramQuoteBalance / ramBaseBalance) * 1024).toFixed(8); // Price in kb
 
     var target = document.getElementById("ram-price-eos");
@@ -111,6 +110,7 @@ jQuery(window).load(function($) {
     target.innerHTML = "~ $" + (ramPriceEos * eosPriceUsd).toFixed(3) + " USD per Kb";
 
     /** coming soon
+    var ramUsed = 1 - (ramBaseBalance - maxRam);
     var target = document.getElementById("rampriceeos");
     target.innerHTML = ramPriceEos + " EOS";
     target = document.getElementById("rampriceusd");
