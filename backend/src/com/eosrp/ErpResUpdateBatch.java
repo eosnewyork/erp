97a2e5adf4c0324d790dd7b967d1851847e296bf
acoutts <andrewjohncoutts@gmail.com>
2018-06-17 08:55:03 -0400

    First test of all 3 write operations
    All 3 metrics writing successfully to the database.

--- backend/src/com/eosrp/ErpResUpdateBatch.java
+++ backend/src/com/eosrp/ErpResUpdateBatch.java
@@ -1,7 +1,6 @@
 package com.eosrp;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.eosrp.db.DbContract;
 import com.eosrp.db.PostgresHelper;
@@ -29,31 +28,69 @@ public class ErpResUpdateBatch {
 		return client;
 	}
 
-	public static void updateRamPrice(EosResources _eosRes, PostgresHelper _client, String _strUrlEosPriceUsd, String _strUrlGetTableRows) throws SQLException, IOException {
-
-
-
-		if (_client != null) {
-			Boolean result = _client.sendQuery("eosram", _eosRes.getEosPriceUsd(_strUrlEosPriceUsd), _client, _eosRes.getRamBytesPrice(_strUrlGetTableRows));
-			if (result == true) {
-				//~ DEBUG
-				//~ System.out.println("Successful query");
+	public static void storePrice(String _table, EosResources _eosRes, PostgresHelper _client, String _strUrlEosPriceUsd, String _strUrlGetTableRows) throws SQLException, IOException {
+		Boolean result;
+		
+		//~ DEBUG
+		//~ System.out.println(_table);
+		
+		switch (_table) {
+		case "eosram":
+			if (_client != null) {
+				result = _client.sendQuery("eosram", _eosRes.getEosPriceUsd(_strUrlEosPriceUsd), _client, _eosRes.getRamBytesPrice(_strUrlGetTableRows));
+				if (result == true) {
+					//~ DEBUG
+					//~ System.out.println("Successful query");
+				}
+				else
+				{
+					//~ DEBUG
+					//~ System.out.println("Unsuccessful query");
+				}
 			}
-			else
-			{
-				//~ DEBUG
-				//~ System.out.println("Unsuccessful query");
+			break;
+			
+		case "eoscpu":
+			if (_client != null) {
+				result = _client.sendQuery("eoscpu", _eosRes.getEosPriceUsd(_strUrlEosPriceUsd), _client, _eosRes.getCpuMicSecPrice(_strUrlGetTableRows));
+				if (result == true) {
+					//~ DEBUG
+					//~ System.out.println("Successful query");
+				}
+				else
+				{
+					//~ DEBUG
+					//~ System.out.println("Unsuccessful query");
+				}
 			}
+			break;
+			
+		case "eosnet":
+			if (_client != null) {
+				result = _client.sendQuery("eosnet", _eosRes.getEosPriceUsd(_strUrlEosPriceUsd), _client, _eosRes.getNetBandBytesPrice(_strUrlGetTableRows));
+				if (result == true) {
+					//~ DEBUG
+					//~ System.out.println("Successful query");
+				}
+				else
+				{
+					//~ DEBUG
+					//~ System.out.println("Unsuccessful query");
+				}
+			}
+			break;
 		}
+		
+
 
 		//~ Print the DB records
-		ResultSet rs = _client.execQuery("SELECT * FROM eosram");
+		/*ResultSet rs = _client.execQuery("SELECT * FROM eosram");
 		while(rs.next()) {
 			System.out.printf("%s\t%s\t%s\n", 
 					rs.getString(1),
 					rs.getString(2),
 					rs.getString(3));
-		}
+		}*/
 	}
 
 	public static void main(String[] args) throws SQLException, IOException {
@@ -61,14 +98,14 @@ public class ErpResUpdateBatch {
 		String strUrlGetAccount = "http://api.eosnewyork.io/v1/chain/get_account";
 		String strUrlEosPriceUsd = "https://api.coinmarketcap.com/v2/ticker/1765";
 		String strUrlGetTableRows = "http://api.eosnewyork.io/v1/chain/get_table_rows";
-		
+
 		//~ Instantiate objects
 		PostgresHelper client = initDatabase();
 		EosResources eosRes = new EosResources(strUrlEosPriceUsd, strUrlGetTableRows);
 
-		updateRamPrice(eosRes, client, strUrlEosPriceUsd, strUrlGetTableRows);
-		//~ getCpuMicSecPrice(strUrlGetAccount);
-		System.out.println(eosRes.getNetBandBytesPrice(strUrlGetAccount));
+		storePrice("eosram", eosRes, client, strUrlEosPriceUsd, strUrlGetTableRows);
+		storePrice("eoscpu", eosRes, client, strUrlEosPriceUsd, strUrlGetAccount);
+		storePrice("eosnet", eosRes, client, strUrlEosPriceUsd, strUrlGetAccount);
 	}
 
 }
